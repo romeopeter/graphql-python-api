bc040146f74140c14e6ba26a3f62d69903feabaa
Romeo-Peter <romeomeshack34@gmail.com>
2021-10-16 02:38:14 +0100

    Updated with database configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
-from flas_cors import CORS
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import dotenv_values
 
 server = Flask(__name__)
-
 # Allows Cross-Orgin Resorce Sharing (CORS)
 CORS(server)
 
-# Views
-# import app.views
+# Loads DB env variables
+config = dotenv_values(".flaskenv")
+
+
+server.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://{config['DB_USERNAME']}:{config['DB_PASSWORD']}@fanny.db.elephantsql.com/vgunlndd"
+server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(server)
+
+# Views & models
+import app.views
+
+import app.models
